1471e2e12d2a31a79d5eafbbf2c84b16dde48940
masab-abdullah <m.masababdullah@gmail.com>
2026-06-11 13:23:16 +0500

    done

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
+
 from fastapi import FastAPI
+from fastapi import users 
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Welcome Abdullah"}
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
-
-@app.get("/about")
-def about():
-    return {"message": "About Page"}
-
-@app.get("/student")
-def student():
-    return {
-        "name": "Abdullah",
-        "age": 18
-    }
-@app.get("/info")
-def api_info():
-    return {
-        "name": "My First FastAPI Project",
-        "version": "1.0",
-        "description": "This API is created for learning FastAPI basics",
-        "author": "Abdullah",
-        "status": "running"
-    }
+app.include_router(users.router) 
