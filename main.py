01737ff543f3313c852e1708623e04bf410c03ff
masab-abdullah <m.masababdullah@gmail.com>
2026-06-11 11:22:51 +0500

    done

--- main.py
+++ main.py
@@ -10,6 +10,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
 @app.get("/")
 def home():
-    return {"message": "API is working!"}
+    return {"message": "Hello World"}
+
+@app.get("/about")
+def about():
+    return {"message": "About Page"}
+
+@app.get("/student")
+def student():
+    return {
+        "name": "Abdullah",
+        "age": 18
+    }
+@app.get("/info")
+def api_info():
+    return {
+        "name": "My First FastAPI Project",
+        "version": "1.0",
+        "description": "This API is created for learning FastAPI basics",
+        "author": "Abdullah",
+        "status": "running"
+    }
