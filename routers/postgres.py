cc9bc8d2394a75cfa7e5df057d2a5b7146dd4095
masab-abdullah <m.masababdullah@gmail.com>
2026-06-22 11:41:13 +0500

    done

--- routers/postgres.py
+++ routers/postgres.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
-from database.postgres_connection import test_postgres_connection
+from database.postgres_connection import (
+    test_postgres_connection,
+    PostgresSessionLocal
+)
 
+from sqlalchemy import text
 router = APIRouter()
 
 @router.get("/postgres-check")
@@ -10,3 +14,15 @@ def postgres_check():
     return {
         "database": result[0]
     }
+@router.get("/postgres-students")
+def get_postgres_students():
+    db = PostgresSessionLocal()
+
+    try:
+        result = db.execute(text("SELECT * FROM student"))
+        rows = result.fetchall()
+
+        return [dict(row._mapping) for row in rows]
+
+    finally:
+        db.close()
